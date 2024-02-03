ebf332030361be4c9271c75848662bef99d71ea6
Vander Gerald <vander.gerald@ui.ac.id>
2024-02-03 15:50:27 +0700

    Add functional test for create-product

---
+++ src/test/java/id/ac/ui/cs/advprog/eshop/functional/CreateProductFunctionalTest.java
@@ -0,0 +1,63 @@
+package id.ac.ui.cs.advprog.eshop.functional;
+
+import io.github.bonigarcia.seljup.SeleniumJupiter;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+import static org.springframework.test.util.AssertionErrors.assertTrue;
+
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+@ExtendWith(SeleniumJupiter.class)
+class CreateProductFunctionalTest {
+    @LocalServerPort
+    private int serverPort;
+
+    @Value("${app.baseUrl:http://localhost}")
+    private String testBaseUrl;
+    private String baseUrl;
+
+    @BeforeEach
+    void setupTest() {
+        baseUrl = String.format("%s:%d" + "/product/create", testBaseUrl, serverPort);
+    }
+    @Test
+    void createPageTitle_isCorrect(ChromeDriver driver) throws Exception {
+        driver.get(baseUrl);
+        String pageTitle = driver.getTitle();
+
+        assertEquals("Create New Product", pageTitle);
+    }
+
+    void createPageHeading_isCorrect(ChromeDriver driver) throws Exception {
+        driver.get(baseUrl);
+        String pageHeading = driver.findElement(By.tagName("h3")).getText();
+        assertEquals("Create New Product", pageHeading);
+    }
+
+    @Test
+    void test_createProduct(ChromeDriver driver) throws Exception {
+        driver.get(baseUrl);
+        driver.findElement(By.id("nameInput")).sendKeys("Functional Test Create");
+        driver.findElement(By.id("quantityInput")).sendKeys("20");
+        driver.findElement(By.className("btn-primary")).click();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        assertEquals(String.format("%s:%d" + "/product/list", testBaseUrl, serverPort),driver.getCurrentUrl());
+        String newPageTitle = driver.getTitle();
+        assertEquals("ADV Shop Product List", newPageTitle);
+        String newPageSource = driver.getPageSource();
+        assertTrue("The text should contain",newPageSource.contains("Functional Test Create"));
+        assertTrue("The text should contain",newPageSource.contains("20"));
+    }
+}
