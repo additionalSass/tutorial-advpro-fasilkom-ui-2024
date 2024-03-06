a7815c0d838da0f7fcd30755bbc9c27c45d75d36
Vander Gerald <vander.gerald@ui.ac.id>
2024-03-06 11:41:20 +0700

    [RED] have Added Order model skeleton.

--- src/test/java/id/ac/ui/cs/advprog/eshop/model/OrderTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/OrderTest.java
@@ -30,7 +30,6 @@ class OrderTest {
     @Test
     void testCreateOrderEmptyProduct() {
         this.products.clear();
-
         assertThrows(IllegalArgumentException.class, () -> {
             Order order = new Order("13652556-012a-4c07-b546-54eb1396d79b",
                     this.products, 1708560000L, "Safira Sudrajat");
